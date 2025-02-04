0f66abb872465bb786d90e625e9f5f0cd3a06e94
Justin Saye <jzsaye@crimson.ua.edu>
2025-02-04 16:12:06 -0600

    Added game engine. Integrated with tournament.py.

--- tournament.py
+++ tournament.py
@@ -1,38 +1,7 @@
 import random
 from itertools import combinations
+from game_engine import RandomPlayer, Game
 #Has built in function to return cross product
-
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.wins = 0
-
-    def __str__(self):
-        return self.name
-    
-    def inc(self):
-        self.wins += 1
-        
-    def score(self):
-        return self.wins
-    
-class Scoreboard:
-    def __init__ (self, players):
-        self.players = players
-                    
-    def out(self):
-        for player in players:
-            print(f"Player {player.__str__()} score: {player.score()}")
-
-class Game:
-    def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
-
-    def play(self):
-        #Simulates a game
-        winner = random.choice([self.player1, self.player2])
-        return winner
 
 class Tournament:
     def __init__(self, players, games_per_match):
@@ -46,12 +15,10 @@ class Tournament:
         for _ in range(self.games_per_match):
             for player1, player2 in matchups:
                 game = Game(player1, player2)
-                winner = game.play()
-                winner.inc()
+                winner = game.start()
                 print(f"{player1} vs {player2} winner: {winner}")
 
-players = [Player("A"), Player("B"), Player("C"), Player("D"), Player("T")]
-tournament = Tournament(players, games_per_match=100)
-tournament.run()
-scoreboard = Scoreboard(players)
-scoreboard.out()
+if __name__ == '__main__':
+    players = [RandomPlayer("A"), RandomPlayer("B"), RandomPlayer("C"), RandomPlayer("D"), RandomPlayer("T")]
+    tournament = Tournament(players, games_per_match=3)
+    tournament.run()
